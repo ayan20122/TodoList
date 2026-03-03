25363a900579cfe94750f75fd2ed6179dfae464a
ayan20122 <122.khan.ayan.123@gmail.com>
2026-03-03 19:37:17 +0500

    Merge pull request #1 from ayan20122/main
    Ui fix for mobile

--- src/Pages/AllTask.jsx
+++ src/Pages/AllTask.jsx
@@ -3,7 +3,7 @@ import AllTasks from "../Components/Task";
 
 const AllTask = () => {
   return (
-    <div className="text-white h-screen w-full overflow-y-auto flex items-center justify-center">
+    <div className="text-white h-screen w-full overflow-y-auto bg-[#0a0a0a] pt-10 pb-20">
       <AllTasks showTaskList={true} simpleMode={true} Heading={"Tasks"} />
     </div>
   );
